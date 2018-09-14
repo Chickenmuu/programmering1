3d8fac0a7dafdd123059528139d0e207543d7ef9
Raiwin.S <raiwin7@gmail.com>
2018-09-14 15:07:31 +0200

    Skapade variabel

---
+++ src/LäxaV37.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LäxaV37{ 
+
+public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		int tall = input.nextInt();
+		int min = tall;
+		int max = tall;
+
+		int tal2 = input.nextInt();
+		int tal3 = input.nextInt();
+		int tal4 = input.nextInt();
+		int tal5 = input.nextInt();
+		int tal6 = input.nextInt();
+		int tal7 = input.nextInt();
+		int tal8 = input.nextInt();
+		int tal9 = input.nextInt();
+		int tal10 = input.nextInt();
+	
+
+
+
+
+
+
+}
+
+
+
+
+}
