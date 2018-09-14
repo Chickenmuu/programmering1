7561f57080637d87227432b96ef629d19c0e04ff
Raiwin.S <raiwin7@gmail.com>
2018-09-14 14:45:56 +0200

    användarens information

--- src/ProgrammeringLäxa1.java
+++ src/ProgrammeringLäxa1.java
@@ -2,8 +2,28 @@ import java.util.Scanner;
 
 public class ProgrammeringLäxa1 {
 
-public static void main(String[] args) {
-	
-	Scanner input = new Scanner(System.in);
-}	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Vad heter du?");
+		String name = input.nextLine();
+
+		System.out.println("Age?");
+		String age = input.nextLine();
+
+		System.out.println("Adress?");
+		String adress = input.nextLine();
+
+		System.out.println("Postnummer?");
+		String Postnummer = input.nextLine();
+		
+		System.out.println("Ort?");
+		String Ort = input.nextLine();
+		
+		System.out.println("Telefonnummer?");
+		String Telefonnummer = input.nextLine();
+		
+				
+	}
 }
