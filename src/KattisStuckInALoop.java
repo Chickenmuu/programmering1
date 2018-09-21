75e7c164bb64253ae8303e38d448c108a2fe2f74
Raiwin.S <raiwin7@gmail.com>
2018-09-21 15:17:02 +0200

    Stuck In Loop

---
+++ src/KattisStuckInALoop.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class KattisStuckInALoop {
+	
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+	
+			int x =1;
+			int I = input.nextInt();
+		
+		 while (x <= I) {
+		
+		 System.out.println(x + " Abracadabra");
+		 x++;
+		 }
+		 }
+}
